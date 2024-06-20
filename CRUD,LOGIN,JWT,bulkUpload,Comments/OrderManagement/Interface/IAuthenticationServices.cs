b514e83d6df7002908aa23a67ee0e6540601dbb9
elizabath-gigi <elizabath.gigi@people10.com>
2024-06-20 15:59:53 +0530

    CRUD+LOGIN+JWT+BULKUPLOAD+COMMENTS

---
+++ CRUD,LOGIN,JWT,bulkUpload,Comments/OrderManagement/Interface/IAuthenticationServices.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using OrderManagement.DTOs;
+using OrderManagement.Models;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace OrderManagement.Interface
+{
+    public interface IAuthenticationServices
+    {
+        public Task<List<User>> GetUsers();
+        public Task<User> Register(RegisterRequestDto request);
+        public Task<User> Login(LoginRequestDto request);
+        public Task<string> Reset(ResetRequestDto request);
+        public JwtSecurityToken GenerateJSONWebToken(User user);
+        
+    }
+}
